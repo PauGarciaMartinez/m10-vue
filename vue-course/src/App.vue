0ebc2cbf7406f9758f37d41241fb7bc543b6860b
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-09 18:06:27 +0100

    Create all files

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -3,24 +3,6 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/javascript" src="./scripts/app.js" ></script>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/css/style.css"></style>
