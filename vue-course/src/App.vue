c289234f8cf849d7de06345c79fdf952b336b79b
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-10 18:55:54 +0100

    Create files

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,8 +1,14 @@
+<!-- TEMPLATE -->
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+<!-- SCRIPT -->
+
 <script type="text/javascript" src="./scripts/app.js" ></script>
 
-<style src="./styles/css/style.css"></style>
+<!-- STYLES -->
+
+<style src="./styles/css/app.css"></style>
