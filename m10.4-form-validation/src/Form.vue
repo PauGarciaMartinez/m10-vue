3311107c462b45bea2313de8ee144c0376b7bc67
Pau Garcia Martínez <etpau@hotmail.es>
2021-02-13 21:09:19 +0100

    Establish validations

--- m10.4-form-validation/src/Form.vue
+++ m10.4-form-validation/src/Form.vue
@@ -1,21 +1,27 @@
 <!-- TEMPLATE -->
 
 <template>
-  <h1>Form VUElidation</h1>
+  <h1>Form <span>VUE</span>lidation</h1>
   <form>
     <label>Name</label>
-    <input type="text" required>
+    <input type="text" required v-model="name" :class="{invalid: nameError}">
+    <small v-if="name">{{ dynamicVal }}</small>
     <label>Mobile Number</label>
-    <input type="number">
+    <input type="number" required v-model="number" :class="{invalid: numberError}">
+    <small v-if="numberError">{{ numberError }}</small>
     <label>Postal Code</label>
-    <input type="number">
+    <input type="number" required v-model="postcode" :class="{invalid: postcodeError}">
+    <small v-if="postcodeError">{{ postcodeError }}</small>
     <label>Email</label>
-    <input type="email">
+    <input type="email" required v-model="email" :class="{invalid: emailError}">
+    <small v-if="emailError">{{ emailError }}</small>
     <label>Password</label>
-    <input type="password">
+    <input type="password" required v-model="password" :class="{invalid: passwordError}">
+    <small v-if="passwordError">{{ passwordError }}</small>
     <label>Confirm Password</label>
-    <input type="password">
-    <button type="submit">Submit</button>
+    <input type="password" required v-model="passwordTwo" :class="{invalid: passwordTwoError}">
+    <small v-if="passwordTwoError">{{ passwordTwoError }}</small>
+    <button type="submit" @click.prevent="submitForm">Submit</button>
   </form>
 </template>
 
--- m10.4-form-validation/src/scripts/form.js
+++ m10.4-form-validation/src/scripts/form.js
@@ -2,7 +2,55 @@ import Validation from "./../components/Validation.vue"
 
 export default {
   name: 'Form',
-  components: {
-    Validation
+  components: { Validation },
+  data() {
+    return {
+      name: "",
+      number: "",
+      postcode: "",
+      email: "",
+      password: "",
+      passwordTwo: "",
+
+      nameError: "",
+      numberError: "",
+      postcodeError: "",
+      emailError: "", 
+      passwordError: "",
+      passwordTwoError: "",
+
+      nameRegex: /[ña-z]{6,13}/gi,
+      numberRegex: /[0-9]/g,
+      emailRegex: /([a-zñ\d.-]+)@([a-zñ\d.-]+)\.([a-z]{2,8})/gi,
+      passwordRegex: /^(?=.*[ÑA-Z])(?=.*[0-9])(?=.{8,})/
+    }
+  },
+  methods: {
+    submitForm() {
+      this.name.match(this.nameRegex) ? this.nameError = "" : 
+        this.nameError = "Name must be between 6 and 13 letters"
+
+      this.number.match(this.numberRegex) ? this.numberError = "" : 
+        this.numberError = "Mobile Number must be only numbers"
+      
+      this.postcode.match(this.numberRegex) ? this.postcodeError = "" : 
+        this.postcodeError = "Postcode must be only numbers"
+      
+      this.email.match(this.emailRegex) ? this.emailError = "" : 
+        this.emailError = "Please introduce a valid email"
+      
+      this.password.match(this.passwordRegex) ? this.passwordError = "" : 
+        this.passwordError = "Password must contain one cap, number and special character"
+      
+      this.passwordTwo !== this.password ? this.passwordTwoError = "" : 
+        this.passwordTwoError = "Passwords don't match"
+    }
+  },
+  computed: {
+    dynamicVal() {
+      this.name.match(this.nameRegex) ? this.nameError = "" : 
+        this.nameError = "Name must be between 6 and 13 letters"
+      return this.nameError
+    }
   }
 }
